d3954c0ee95793268df70d8ef6e1d5674ff91116
ilqarsfv <ilqar.sofiyev.95@gmail.com>
2025-07-25 11:58:58 +0400

    v1.5

--- backend/server.js
+++ backend/server.js
@@ -6,21 +6,18 @@ const productRoutes = require("./routes/product.route");
 const path = require("path");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-const _dirname = path.resolve();
-
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "/frontend/dist")));
+  app.use(express.static(path.join(path.resolve(), "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(path.resolve(), "frontend", "dist", "index.html"));
   });
 }
 
 app.listen(PORT, () => {
-  console.log("Server is running");
   connectDB();
 });
